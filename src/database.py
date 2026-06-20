c3ea9f34ea08b21390ec6522942a52f00085de4f
keshu <alokikgarg24@gmail.com>
2026-06-20 22:53:12 +0530

    fix: render asyncpg url

--- src/database.py
+++ src/database.py
@@ -9,6 +9,13 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if DATABASE_URL:
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgresql://",
+        "postgresql+asyncpg://",
+        1,
+    )
+
 if not DATABASE_URL:
     DATABASE_URL = (
         f"postgresql+asyncpg://"
